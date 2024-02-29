f55aafe6e06f736930feca6ddfda318acdb70718
SKRLIK, MARTIN <martin.skrlik@accenture.com>
2024-02-29 09:16:03 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+
+
+print("Hello world!")
+a = 14
+print(a)
+a = 4.55
+print(a)
+print(type(a))
+
---
+++ parrot.py
@@ -0,0 +1,6 @@
+print("Please input your name")
+print("Your name")
+
+vstup = input()
+vystup = vstup + ", " + vstup
+print(vystup)
