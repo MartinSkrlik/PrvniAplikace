1149d570f6b80192314774384bee100545713bf2
SKRLIK, MARTIN <martin.skrlik@accenture.com>
2024-03-05 16:33:26 +0100

    functions

---
+++ mathFunction.py
@@ -0,0 +1,16 @@
+import math
+
+"""
+print("Pí:", math.pi)
+print("e:", math.e)
+print(math.sin(math.pi/2))  # Vrací sinus 90°
+print(math.degrees(math.pi))   # Vrací 180.0
+print(math.radians(180))  
+
+
+
+
+
+"""
+
+
